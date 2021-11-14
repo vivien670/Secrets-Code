d663224b1de4ee9c5c9a65cbb725c7ca75b4f216
Your Name <you@example.com>
2021-11-14 16:30:21 +0100

    Level 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
